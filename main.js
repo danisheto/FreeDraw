77051413c8158842b3c4b72f9af1030e89bf7517
Danisheto <danisheto@gmail.com>
2014-04-18 17:50:39 -0500

    In the Beginning, the second time

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
 var canvas=document.getElementById("canvas"),
-	context=canvas.getContext("2d");
+	context=canvas.getContext("2d"),
+	posX,posY;
+function findPos(){
+	posX=e.pageX-this.offsetLeft;
+	posY=e.pageY-this.offsetTop;
+}
+canvas.onmousedown=function(e){
+	findPos();
+	context.beginPath();
+	context.moveTo(posX,posY);
+	canvas.onmousemove=function(e){
+		findPos();
+		context.moveTo(posX,posY);
+		context.fillStyle="#000000",
+		context.fill();
+	}
+	canvas.onmouseup=function(){
+		context.stroke();
+		canvas.onmousemove=false;
+	}
+}
