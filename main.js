6f88885fe927cf729e56bf9c67146af9df13c0b3
Danisheto <danisheto@gmail.com>
2014-04-19 12:30:32 -0500

    made the canvas responsive

--- main.js
+++ main.js
@@ -1,23 +1,18 @@
-function id(str){
-	return document.getElementById(str);
-}
-//color picker css
-smallColor.onclick=function(){
-	smallColor.className="";
-	smallColorImg.style.width="100%";
-	smallColorImg.style.height="100%";
-	smallColorExtra.style.display="inline-block";
-}
-smallColorDone.onclick=function(){
-	smallColor.className="tools";
-	smallColorImg.style.width="16px";
-	smallColorImg.style.height="16px";
-	smallColorExtra.style.display="none";
-}
-
-
 var canvas=document.getElementById("canvas"),posX,posY;
 var context=canvas.getContext("2d");
+window.onresize=function(){
+	// canvas.setAttribute('width',window.innerWidth-10);
+	// canvas.setAttribute("height",window.innerHeight-27-30);
+	context.canvas.width=window.innerWidth
+	context.canvas.height=window.innerWidth-27;
+}
+window.onresize.apply();
+//color picker css
+smallColor.onclick=function(){
+	colorpickerLarge.style.display="block"
+}
+
+
 canvas.onmousedown=function(e){
 	posX=e.pageX-this.offsetLeft;
 	posY=e.pageY-this.offsetTop;
@@ -29,8 +24,16 @@ canvas.onmousedown=function(e){
 		context.lineTo(posX,posY);
 		context.stroke();
 	}
-	canvas.onmouseup=function(){
+	function stopDrawing(){
 		context.closePath();
 		canvas.onmousemove=false;
 	}
+	canvas.onmouseup=function(){
+		context.closePath();
+		canvas.onmousemove=false;
+	};
+	canvas.onmouseout=function(){
+		context.closePath();
+		canvas.onmousemove=false;
+	};
 }
