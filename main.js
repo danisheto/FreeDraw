d1313d14711b2d021d92f6f3d591a88e05e29315
Danisheto <danisheto@gmail.com>
2014-04-19 20:28:58 -0500

    completed colorpicker, needs rgb support

--- main.js
+++ main.js
@@ -1,4 +1,43 @@
-var canvas=document.getElementById("canvas"),canvasPosX,canvasPosY,colorPosX,colorPosY,huePos,hueBarX;
+var canvas=document.getElementById("canvas"),canvasPosX,canvasPosY,colorPosX,colorPosY,huePos,hueBarX,hue,saturation,brightness,lightness, saturationHSL;
+hue=360;
+saturationHSL=100;
+brightness=0;
+lightness=0;
+saturation=100;
+function satBriPicker(){
+	if(colorPosX<=256 && colorPosX>=0 && colorPosY<=256 && colorPosY>=0){
+		saturation=(colorPosX/256*100).toFixed(0);
+		brightness=100-(colorPosY/256*100).toFixed(0);
+		lightness=(2-(saturation/100))*(brightness/100)*50;
+		saturationHSL=(saturation/100)*(brightness/100)/(((lightness/50)<=1)?(lightness/50):2-(lightness/50));
+		color.style.backgroundColor="hsl("+hue+","+(saturationHSL*100)+"%,"+(lightness)+"%)";
+		saturationInput.value=saturation;
+		brightnessInput.value=brightness;
+		selectedColor.style.right=256-colorPosX+10+"px";
+		selectedColor.style.bottom=256-colorPosY-5+"px";
+	}else{
+		stopMovingSelector();
+	}
+}
+function huePicker(){
+	if(huePos>=0 && huePos<=256 && huePosX>=1 && huePosX<=20){
+		hue=(huePos/256*360).toFixed(0);
+		hueInput.value=hue;
+		color.style.backgroundColor="hsl("+hue+","+(saturationHSL*100)+"%,"+(lightness)+"%)";
+		smallColorImg.style.backgroundColor="hsl("+hue+",100%,50%)";
+		SatBri.style.backgroundColor="hsl("+hue+",100%,50%)";
+		document.getElementsByClassName("arrow")[0].style.bottom=256-huePos-5+"px";
+		document.getElementsByClassName("arrow")[1].style.bottom=256-huePos-5+"px";
+	}else{
+		stopMovingHueBar();
+	}
+}
+function stopMovingSelector(){
+	SatBriSpan.onmousemove=null;
+}
+function stopMovingHueBar(){
+	hueSection.onmousemove=null;
+}
 var context=canvas.getContext("2d");
 window.onresize=function(){
 	context.canvas.width=window.innerWidth
@@ -11,45 +50,30 @@ smallColor.onclick=function(){
 	SatBriSpan.onmousedown=function(e){
 		colorPosX=e.pageX-this.offsetLeft-42;
 		colorPosY=e.pageY-this.offsetTop-39;
-		selectedColor.style.right=256-colorPosX+10+"px";
-		selectedColor.style.bottom=256-colorPosY-5+"px";
+		satBriPicker(e);
 		SatBriSpan.onmousemove=function(e){
 			colorPosX=e.pageX-this.offsetLeft-42;
 			colorPosY=e.pageY-this.offsetTop-39;
-			if(colorPosX<=256 && colorPosX>=0 && colorPosY<=256 && colorPosY>=0){
-				selectedColor.style.right=256-colorPosX+10+"px";
-				selectedColor.style.bottom=256-colorPosY-5+"px";
-			}else{
-				stopMovingSelector();
-			}
-		}
-		function stopMovingSelector(){
-			SatBriSpan.onmousemove=null;
+			satBriPicker(e);
 		}
 		SatBriSpan.onmouseup=stopMovingSelector;
 	}
 	hueBar.onmousedown=function(e){
 		huePos=e.pageY-this.offsetTop-39;
-		huePosX=e.pageX-hue.offsetLeft;
-		document.getElementsByClassName("arrow")[0].style.bottom=256-huePos-5+"px";
-		document.getElementsByClassName("arrow")[1].style.bottom=256-huePos-5+"px";
-		hue.onmousemove=function(e){
+		huePosX=e.pageX-hueSection.offsetLeft-54;
+		huePicker();
+		hueSection.onmousemove=function(e){
 			huePos=e.pageY-this.offsetTop-39;
-			huePosX=e.pageX-hue.offsetLeft-54;
-			if(huePos>=0 && huePos<=256 && huePosX>=1 && huePosX<=20){
-				document.getElementsByClassName("arrow")[0].style.bottom=256-huePos-5+"px";
-				document.getElementsByClassName("arrow")[1].style.bottom=256-huePos-5+"px";
-			}else{
-				stopMovingHueBar();
-			}
-		}
-		function stopMovingHueBar(){
-			hue.onmousemove=null;
+			huePosX=e.pageX-hueSection.offsetLeft-54;
+			huePicker();
 		}
 		hueBar.onmouseup=stopMovingHueBar;
 	}
+	doneColorPicker.onclick=function(){
+		colorpickerLarge.style.display="none";
+		context.strokeStyle="hsl("+hue+","+(saturationHSL*100)+"%,"+(lightness)+"%)";
+	}
 }
-
 
 canvas.onmousedown=function(e){
 	canvasPosX=e.pageX-this.offsetLeft;
